27884e00524ce5b34ec1c082ed79ee8b24c697d9
fflorat <fflorat@163.com>
2016-08-05 00:44:28 +0800

    input file image preview

--- js/ctrls/OneCtrl.js
+++ js/ctrls/OneCtrl.js
@@ -1,3 +1,11 @@
 app.controller('OneCtrl',['$rootScope','$scope',function($rootScope,$scope){
-
+        $scope.vm={fileread:'',
+            FileUrl:''
+        }
+        $scope.UpdateFile= function (file) {
+            $scope.vm.fileread=file;
+        }
+        $scope.printFile=function(){
+            console.log($scope.vm.fileread);
+        }
 }])
--- js/directives/directives.js
+++ js/directives/directives.js
@@ -10,3 +10,35 @@ app.directive("showList",[function(){
         templateUrl:'templates/list.html'
     }
 }]);
+
+app.directive("fileRead", [function () {
+    return {
+        scope: {
+            fileread: "=",
+            fileurl:"="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                scope.$apply(function () {
+                    scope.fileread = changeEvent.target.files[0];
+                    // or all selected files:
+                    // scope.fileread = changeEvent.target.files;
+                });
+
+                var objUrl;
+                if(navigator.userAgent.indexOf("MSIE")>0){
+                    objUrl = scope.fileread;
+                }else {
+                    objUrl = InputFileOp.GetObjectUrl(scope.fileread);
+                }
+
+                scope.$apply(function () {
+                    scope.fileurl=objUrl;
+                })
+                console.log("objUrl = "+objUrl) ;
+
+
+            });
+        }
+    }
+}]);
